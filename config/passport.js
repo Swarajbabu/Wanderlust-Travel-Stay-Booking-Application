f48840656bace5120a61605c2699407a9e32385e
Vecha Laxmi Swaraj Babu <syamalaswaraj@gmail.com>
2026-08-29 01:41:37 +0530

    Enable proxy: true in GoogleStrategy for Render SSL reverse proxy

--- config/passport.js
+++ config/passport.js
@@ -8,8 +8,9 @@ function configurePassport(passport) {
     passport.use(new GoogleStrategy({
         clientID: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        callbackURL: process.env.GOOGLE_CALLBACK_URL,
-        passReqToCallback: true
+        callbackURL: process.env.GOOGLE_CALLBACK_URL || "/auth/google/callback",
+        passReqToCallback: true,
+        proxy: true
     },
     async function(req, accessToken, refreshToken, profile, done) {
         try {
